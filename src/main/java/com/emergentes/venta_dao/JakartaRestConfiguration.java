9d19d243100f26f61ca97e4badc13a61ccf85c01
Luis Alfredo Choque Canaviri <alfredochoque.c@gmail.com>
2024-05-26 00:01:21 -0400

    inicio de proyecto :)

---
+++ src/main/java/com/emergentes/venta_dao/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.emergentes.venta_dao;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
